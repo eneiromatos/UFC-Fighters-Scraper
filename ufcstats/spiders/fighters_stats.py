15e22a4657924018e15754da477e9e551c5799a5
Eneiro Matos <eneiromatos@gmail.com>
2022-06-27 22:22:49 -0400

    Add bio_fields, add parse_str_target

--- ufcstats/spiders/fighters_stats.py
+++ ufcstats/spiders/fighters_stats.py
@@ -19,7 +19,7 @@ class FightersStatsSpider(scrapy.Spider):
         next_url = [response.urljoin(url.get()) for url in next_url]
 
         yield from response.follow_all(athletes, self.parse_fighter_bio)
-        # yield from response.follow_all(next_url, self.parse_athletes_listing)
+        yield from response.follow_all(next_url, self.parse_athletes_listing)
 
     def parse_fighter_bio(self, response):
         item = items.FighterBioItemLoader(selector=response)
@@ -33,12 +33,31 @@ class FightersStatsSpider(scrapy.Spider):
             "division",
             '.l-main__hero .c-hero__header div[class*="headline-suffix"]::text',
         )
-        item.add_css(
-            "hometown",
-            '.c-bio__row--1col div[class*="border-bottom"] .c-bio__text::text',
-        )
-        item.add_value("fighter_stats", self.parse_main_fighter_stats(response))
 
+        bio_fields = response.css(".c-bio__info-details .c-bio__field")
+        for field in bio_fields:
+            if "Status" == field.css(".c-bio__label::text").get():
+                item.add_value("status", field.css(".c-bio__text::text").get())
+            if "Hometown" == field.css(".c-bio__label::text").get():
+                item.add_value("hometown", field.css(".c-bio__text::text").get())
+            if "Trains at" == field.css(".c-bio__label::text").get():
+                item.add_value("trains_at", field.css(".c-bio__text::text").get())
+            if "Fighting style" == field.css(".c-bio__label::text").get():
+                item.add_value("fighting_style", field.css(".c-bio__text::text").get())
+            if "Age" == field.css(".c-bio__label::text").get():
+                item.add_value("age", field.css(".c-bio__text .field::text").get())
+            if "Height" == field.css(".c-bio__label::text").get():
+                item.add_value("height", field.css(".c-bio__text::text").get())
+            if "Weight" == field.css(".c-bio__label::text").get():
+                item.add_value("weight", field.css(".c-bio__text::text").get())
+            if "Octagon Debut" == field.css(".c-bio__label::text").get():
+                item.add_value("octagon_debut", field.css(".c-bio__text::text").get())
+            if "Reach" == field.css(".c-bio__label::text").get():
+                item.add_value("reach", field.css(".c-bio__text::text").get())
+            if "Leg reach" == field.css(".c-bio__label::text").get():
+                item.add_value("leg_reach", field.css(".c-bio__text::text").get())
+
+        item.add_value("fighter_stats", self.parse_main_fighter_stats(response))
         yield item.load_item()
 
     def parse_main_fighter_stats(self, response):
@@ -77,7 +96,6 @@ class FightersStatsSpider(scrapy.Spider):
                     "sig_strikes_attempted",
                     card.css(".c-overlap__stats-value:nth-child(4)::text").get(),
                 )
-
         item.add_value(
             "sig_strikes_landed_per_min",
             stats[0].css('div[class*="number"]::text').get(),
@@ -86,8 +104,8 @@ class FightersStatsSpider(scrapy.Spider):
             "sig_strikes_absorbed_per_min",
             stats[1].css('div[class*="number"]::text').get(),
         )
-        item.add_value('sig_str_by_position', self.parse_str_possition(response))
-
+        item.add_value("sig_str_by_position", self.parse_str_possition(response))
+        item.add_value("sig_str_by_target", self.parse_str_target(response))
         yield item.load_item()
 
     def parse_grappling_stats(self, response):
@@ -115,7 +133,6 @@ class FightersStatsSpider(scrapy.Spider):
             "submission_avg_per_15_min",
             stats[3].css('div[class*="number"]::text').get(),
         )
-
         yield item.load_item()
 
     def parse_str_possition(self, response):
@@ -138,7 +155,11 @@ class FightersStatsSpider(scrapy.Spider):
                 yield item.load_item()
 
     def parse_str_target(self, response):
-        pass
+        item = items.StrTargetItemLoader(selector=response)
+        item.add_css("head", '.c-stat-body text[id*="head_value"]::text')
+        item.add_css("body", '.c-stat-body text[id*="body_value"]::text')
+        item.add_css("leg", '.c-stat-body text[id*="leg_value"]::text')
+        yield item.load_item()
 
     def parse_win_way(self, response):
         graph_stats = response.css(".c-stats-group-3col__item")
